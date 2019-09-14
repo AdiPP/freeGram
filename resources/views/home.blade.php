59cdf5f3289888e0bb7944c43957031ecb5cab05
peps-pvln <adiputrapermana@gmail.com>
2019-09-15 03:16:12 +0700

    git init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fsrg1-1.fna.fbcdn.net/vp/541c98380cb003ecfec3569ea2103465/5E038B38/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fsrg1-1.fna.fbcdn.net" class="rounded-circle" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freeCodeGram</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+        </div>  
+    </div>
+</div>
+@endsection
